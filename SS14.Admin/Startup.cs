a4468118b584c53c322542b6ecaee44b65aeeaa0
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-06-02 22:37:39 +0200

    Reverse proxy stuff in case I need it (not sure tbh).

--- SS14.Admin/Startup.cs
+++ SS14.Admin/Startup.cs
@@ -1,9 +1,10 @@
 using System;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
+using System.Net;
 using Content.Server.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -84,6 +85,24 @@ namespace SS14.Admin
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+
+            var forwardedHeadersOptions = new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+            };
+
+            foreach (var ip in Configuration.GetSection("ForwardProxies").Get<string[]>())
+            {
+                forwardedHeadersOptions.KnownProxies.Add(IPAddress.Parse(ip));
+            }
+
+            app.UseForwardedHeaders(forwardedHeadersOptions);
+
+            var pathBase = Configuration.GetValue<string>("PathBase");
+            if (!string.IsNullOrEmpty(pathBase))
+            {
+                app.UsePathBase(pathBase);
             }
 
             app.UseAuthentication();
