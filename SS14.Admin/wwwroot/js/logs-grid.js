ca6dcaaf4e0e98407a408852bdb917446f04efbe
juliangiebel <juliangiebel@live.de>
2022-05-01 16:43:24 +0200

    Improve filter row layout Improve details sidebar usability

--- SS14.Admin/wwwroot/js/logs-grid.js
+++ SS14.Admin/wwwroot/js/logs-grid.js
@@ -2,17 +2,23 @@
 const overlay = document.getElementById('side-panel-overlay');
 const entries = document.querySelectorAll('.data-log');
 
+let selecting = false;
+
+document.addEventListener('selectstart', () => selecting = true);
 entries.forEach(entry => entry.addEventListener('click', onDataGridEntryClick, true));
 
-overlay.addEventListener('click', () => {
+function onDetailsClose() {
     sidepanel.classList.add('hidden-right');
-    overlay.classList.add('hidden');
-});
+}
 
 function onDataGridEntryClick(e) {
+    if (selecting) {
+        selecting = false;
+        return;
+    }
+
     const template = e.target.parentNode.querySelector('template');
     sidepanel.innerHTML = '';
     sidepanel.appendChild(document.importNode(template.content, true));
     sidepanel.classList.remove('hidden-right');
-    overlay.classList.remove('hidden');
 }
