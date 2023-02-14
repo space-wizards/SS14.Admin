6b76801ca55d1cf0ed61817974e65309b7025365
Morb <14136326+Morb0@users.noreply.github.com>
2023-02-13 16:55:41 -0800

    Import Http

--- SS14.Admin/Startup.cs
+++ SS14.Admin/Startup.cs
@@ -4,6 +4,7 @@ using System.Net;
 using Content.Server.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
