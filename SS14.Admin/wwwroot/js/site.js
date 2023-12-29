33c9a811b00b7880363cfd491a9e17b8c734f8d0
Simon <63975668+Simyon264@users.noreply.github.com>
2023-12-29 23:01:11 +0100

    Toggle for dark mode

--- SS14.Admin/wwwroot/js/site.js
+++ SS14.Admin/wwwroot/js/site.js
@@ -15,3 +15,37 @@ const context = {
         PAGE_INDEX: "pageIndex"
     }
 }
+
+function toggleDarkMode() {
+    let darkModeCookie = getCookie("darkMode");
+    if (darkModeCookie === "" || darkModeCookie === "false") {
+        document.body.classList.add("dark-mode");
+        setCookie("darkMode", "true", 365);
+    } else {
+        document.body.classList.remove("dark-mode");
+        setCookie("darkMode", "false", 365);
+    }
+
+    location.reload();
+}
+
+function getCookie(name) {
+    let cookieName = name + "=";
+    let cookies = document.cookie.split(';');
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+
+    return "";
+}
+
+function setCookie(name, value, days) {
+    let expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + days);
+    let cookieValue = name + "=" + value + ";expires=" + expirationDate.toUTCString() + ";path=/";
+    document.cookie = cookieValue;
+}
