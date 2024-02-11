c150fbb104f1d4811523bc29fd61f58b035a2e27
Geekyhobo <66805063+Ahlytlex@users.noreply.github.com>
2024-02-10 20:36:08 -0500

    i forgor the helper class

--- SS14.Admin/Helpers/YmlHelper.cs
+++ SS14.Admin/Helpers/YmlHelper.cs
@@ -1,6 +1,7 @@
-﻿using YamlDotNet.Serialization;
-using System.IO;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using YamlDotNet.Serialization;
 
 namespace SS14.Admin.Helpers
 {
@@ -13,34 +14,73 @@ namespace SS14.Admin.Helpers
 
             using (var reader = new StreamReader(filePath))
             {
-                var yamlObject = deserializer.Deserialize<Dictionary<string, List<Dictionary<string, object>>>>(reader);
+                var yamlObject = deserializer.Deserialize<RootObject>(reader);
 
-                foreach (var entry in yamlObject["Entries"])
+                if (yamlObject.Entries != null)
                 {
-                    var entryDict = (Dictionary<string, object>)entry;
-
-                    var changeEntry = new ChangeEntry
+                    foreach (var entry in yamlObject.Entries)
                     {
-                        Author = (string)entryDict["author"],
-                        Title = (string)entryDict["title"],
-                        Message = (string)entryDict["message"],
-                        Type = (string)entryDict["type"],
-                        Id = (string)entryDict["id"]
-                    };
-                    entries.Add(changeEntry);
+                        if (entry.Changes != null && entry.Changes.Count > 0)
+                        {
+                            var firstChange = entry.Changes[0];
+
+                            var changeEntry = new ChangeEntry
+                            {
+                                Author = entry.Author,
+                                Message = firstChange.Message,
+                                Type = firstChange.Type,
+                                Id = entry.Id,
+                                Time = entry.Time
+                            };
+                            entries.Add(changeEntry);
+                        }
+                    }
                 }
             }
 
             return entries;
+        }
+
+        // Define the root object structure
+        public class RootObject
+        {
+            [YamlMember(Alias = "Entries")]
+            public List<YamlEntry> Entries { get; set; }
+        }
+
+        // Define the entry object structure
+        public class YamlEntry
+        {
+            [YamlMember(Alias = "author")]
+            public string Author { get; set; }
+
+            [YamlMember(Alias = "id")]
+            public string Id { get; set; }
+
+            [YamlMember(Alias = "time")]
+            public DateTime Time { get; set; }
+
+            [YamlMember(Alias = "changes")]
+            public List<YamlChange> Changes { get; set; }
+        }
+
+        // Define the change object structure
+        public class YamlChange
+        {
+            [YamlMember(Alias = "message")]
+            public string Message { get; set; }
+
+            [YamlMember(Alias = "type")]
+            public string Type { get; set; }
         }
     }
 
     public class ChangeEntry
     {
         public string Author { get; set; }
-        public string Title { get; set; }
         public string Message { get; set; }
         public string Type { get; set; }
         public string Id { get; set; }
+        public DateTime Time { get; set; }
     }
 }
