4103020284bea2f93b6007cc1d4fce842a367f15
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2024-06-23 02:56:21 +0200

    Ban template system
    Requires https://github.com/space-wizards/space-station-14/pull/29365

---
+++ SS14.Admin/Pages/Bans/BanTemplates/Create.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Content.Server.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SS14.Admin.Pages.Bans.BanTemplates;
+
+[Authorize(Roles = "BAN")]
+[ValidateAntiForgeryToken]
+public class Create(PostgresServerDbContext dbContext) : PageModel
+{
+    [BindProperty] public string Title { get; set; } = "";
+
+    public void OnGet()
+    {
+
+    }
+
+    public async Task<IActionResult> OnPostCreateAsync()
+    {
+        if (string.IsNullOrWhiteSpace(Title))
+        {
+            TempData["StatusMessage"] = "Error: name must not be empty";
+            return Page();
+        }
+
+        var template = new BanTemplate
+        {
+            Title = Title,
+        };
+
+        dbContext.BanTemplate.Add(template);
+
+        await dbContext.SaveChangesAsync();
+
+        return RedirectToPage("View", new { id = template.Id });
+    }
+}
--- SS14.Admin/Pages/Bans/Create.cshtml.cs
+++ SS14.Admin/Pages/Bans/Create.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Content.Server.Database;
+using Content.Shared.Database;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using SS14.Admin.Helpers;
 
 namespace SS14.Admin.Pages.Bans
@@ -15,6 +17,10 @@ namespace SS14.Admin.Pages.Bans
         private readonly BanHelper _banHelper;
 
         [BindProperty] public InputModel Input { get; set; } = new();
+
+        public ServerBanExemptFlags ExemptFlags;
+
+        public List<BanTemplate> Templates = [];
 
         public Create(PostgresServerDbContext dbContext, BanHelper banHelper)
         {
@@ -31,14 +37,33 @@ namespace SS14.Admin.Pages.Bans
             public bool UseLatestHwid { get; set; }
             public int LengthMinutes { get; set; }
             [Required] public string Reason { get; set; } = "";
+            [Display(Name = "Delete ban when expired")]
+            public bool AutoDelete { get; set; }
+
+            [Display(Name = "Hidden from player")] public bool Hidden { get; set; }
+            public NoteSeverity Severity { get; set; }
+        }
+
+        public async Task OnGetAsync()
+        {
+            await LoadTemplates();
         }
 
         public async Task<IActionResult> OnPostCreateAsync()
         {
+            await LoadTemplates();
+
+            ExemptFlags = BanExemptions.GetExemptionFromForm(Request.Form);
+
             var ban = new ServerBan();
 
             var ipAddr = Input.IP;
             var hwid = Input.HWid;
+
+            ban.ExemptFlags = ExemptFlags;
+            ban.AutoDelete = Input.AutoDelete;
+            ban.Hidden = Input.Hidden;
+            ban.Severity = Input.Severity;
 
             if (Input.UseLatestHwid || Input.UseLatestIp)
             {
@@ -78,6 +103,35 @@ namespace SS14.Admin.Pages.Bans
             TempData["HighlightNewBan"] = ban.Id;
             TempData["StatusMessage"] = "Ban created";
             return RedirectToPage("./Index");
+        }
+
+        public async Task<IActionResult> OnPostUseTemplateAsync(int templateId)
+        {
+            await LoadTemplates();
+
+            var template = await _dbContext.BanTemplate.SingleOrDefaultAsync(t => t.Id == templateId);
+            if (template == null)
+            {
+                TempData.SetStatusError("That template does not exist!");
+                return Page();
+            }
+
+            // Avoid errors causing params to not be passed through.
+            ModelState.Clear();
+
+            Input.Reason = template.Reason;
+            Input.LengthMinutes = (int)template.Length.TotalMinutes;
+            Input.Severity = template.Severity;
+            Input.AutoDelete = template.AutoDelete;
+            Input.Hidden = template.Hidden;
+            ExemptFlags = template.ExemptFlags;
+
+            return Page();
+        }
+
+        private async Task LoadTemplates()
+        {
+            Templates = await _dbContext.BanTemplate.ToListAsync();
         }
     }
 }
