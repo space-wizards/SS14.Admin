8ef8cad54e62999c229ad25ce7a55c15a8c30d9e
PJB3005 <pieterjan.briers+git@gmail.com>
2025-01-10 02:10:19 +0100

    Some quartz cleanup

--- SS14.Admin/Constants.cs
+++ SS14.Admin/Constants.cs
@@ -9,7 +9,4 @@ public static class Constants
     public const string HostRole = "HOST";
 
     public const string PolicyPersonalDataManagement = "PersonalDataManagement";
-
-    // Quartz stuff
-    public const string QuartzIntervalScheduler = "QuartzIntervalScheduler";
 }
