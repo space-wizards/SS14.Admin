1abbca6d939c1123f6563fa514f9652cb2dbd625
juliangiebel <juliangiebel@live.de>
2025-06-23 00:24:38 +0200

    Change export process item to be a record

--- SS14.Admin/AdminLogs/Export/ExportProcessItem.cs
+++ SS14.Admin/AdminLogs/Export/ExportProcessItem.cs
@@ -1,21 +1,9 @@
 ﻿namespace SS14.Admin.AdminLogs.Export;
 
-public sealed class ExportProcessItem
-{
-    public DateTime? FromDate { get; }
-    public DateTime? ToDate { get; }
-    public int? RoundId { get; }
-
-    public string? Search { get; }
-
-    public bool UseCompression { get; }
-
-    public ExportProcessItem(string? search = null, DateTime? fromDate = null, DateTime? toDate = null, int? roundId = null, bool useCompression = false)
-    {
-        Search = search;
-        FromDate = fromDate;
-        ToDate = toDate;
-        UseCompression = useCompression;
-        RoundId = roundId;
-    }
-}
+public sealed record ExportProcessItem(
+    string? Search = null,
+    DateTime? FromDate = null,
+    DateTime? ToDate = null,
+    int? RoundId = null,
+    bool UseCompression = false
+);
